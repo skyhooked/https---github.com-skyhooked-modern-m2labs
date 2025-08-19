e1322e69bf913287d9f1decce63b0f63763abaa3
cmsupernova <cryptic_nova@yahoo.com>
2025-08-19 09:18:27 -0400

    Add headers() to force dynamic rendering on root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Layout from '@/components/Layout';
 import HomeClient from './HomeClient';
+import { headers } from 'next/headers';
 
 
 export const runtime = 'edge';
 export const dynamic = 'force-dynamic';
 
 export default function Home() {
+  headers(); // Force dynamic rendering
   return <HomeClient />;
 }
